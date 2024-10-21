02447b36cc2a3e6e3b501dd267736c94c6eb0bdd
Mahdiyeh <zafarimahdiyeh@gmail.com>
2024-10-21 20:18:14 +0330

    fix color & text

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,16 +13,21 @@ async function fetchData() {
 
 function displayData(data) {
     const container = document.getElementById('data-container');
-    container.innerHTML = ''; // پاک کردن محتوای قبلی
+    // container.innerHTML = ''; // پاک کردن محتوای قبلی
 
     data.forEach(item => {
         const col = document.createElement('div');
-        col.className = 'col-12 col-sm-6 col-md-3'; // تنظیم عرض ستون
-        col.innerHTML = `<div class="item">ID:${item.id}<img src="${item.image.thumbnail}" 
+        col.className = 'col-12 col-sm-6 col-md-3 bg-pink rounded-2 p-1'; // تنظیم عرض ستون
+        col.innerHTML = `<div class="row"><img class="rounded-me" src="${item.image.thumbnail}" 
         srcset="${item.image.mobile} 576w, ${item.image.tablet} 768w, ${item.image.desktop} 1200w" 
         sizes="(max-width: 576px) 100vw, (max-width: 768px) 50vw, 33vw" 
         alt="${item.name}" 
-        class="img-fluid"> Name: ${item.name}</div>`;
+        class="img-fluid"></div>
+        <div class="col-12 text-start text-white ps-3 mt-2 ">
+            <div class="row text-333 fst-normal mt-1">${item.category}</div>
+            <div class="row mt-1 fw-bold text-dark">${item.name}</div>
+            <div class="row mt-1 fw-bold text-333">$ ${item.price}</div>
+        </div>`;
         container.appendChild(col);
     });
 }
